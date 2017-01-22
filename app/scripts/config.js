533bb097f2912f31d2b2a9316573a56022c504c0
Edoardo Spadoni <edoardo.spadoni@gmail.com>
2017-01-22 16:04:44 +0100

    added MAP_ZOOM option in config

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,7 @@
 var appConfig = {
     MAP_TOKEN: 'YOUR-MAPBOX-TOKEN',
     MAP_CENTER: [20.9027835, 44.4963655],
+    MAP_ZOOM: 2,
     MAP_STYLE: 'mapbox://styles/mapbox/dark-v9',
     MAP_API_SOURCE: 'YOUR-BACKEND-API-URL'
 };
